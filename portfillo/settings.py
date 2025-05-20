1fc323a7db47e905db48ff5ba4e70733da55cd26
villenbhai <pk6299160241@gmail.com>
2025-05-20 19:39:09 +0530

    Removed .env from tracking and added to .gitignore

--- portfillo/settings.py
+++ portfillo/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 from decouple import config
+from dotenv import load_dotenv
+load_dotenv()
 import os
 import sys
 from pathlib import Path
